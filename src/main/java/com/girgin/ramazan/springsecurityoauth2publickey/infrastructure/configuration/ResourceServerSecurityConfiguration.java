eb7aa4c25eecd3f4c18618fc3be0ac9d53f35e46
Ramazan Girgin <ramazangirgin@gmail.com>
2019-01-23 02:46:05 +0100

    Converts to Spring Cloud Security

--- src/main/java/com/girgin/ramazan/springsecurityoauth2publickey/infrastructure/configuration/ResourceServerSecurityConfiguration.java
+++ src/main/java/com/girgin/ramazan/springsecurityoauth2publickey/infrastructure/configuration/ResourceServerSecurityConfiguration.java
@@ -1,67 +1,23 @@
 package com.girgin.ramazan.springsecurityoauth2publickey.infrastructure.configuration;
 
-import com.girgin.ramazan.springsecurityoauth2publickey.infrastructure.SecurityProperties;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
 
 @Configuration
+@EnableResourceServer
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class ResourceServerSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    SecurityProperties securityProperties;
+public class ResourceServerSecurityConfiguration extends ResourceServerConfigurerAdapter {
 
     @Override
-    public AuthenticationManager authenticationManager() {
-        OAuth2AuthenticationManager authManager = new OAuth2AuthenticationManager();
-        authManager.setTokenServices(tokenService());
-        return authManager;
-    }
-
-    @Bean
-    ResourceServerTokenServices tokenService() {
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        defaultTokenServices.setSupportRefreshToken(false);
-        return defaultTokenServices;
-    }
-
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
-
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setVerifierKey(securityProperties.getJwtVerificationKey());
-        return converter;
-    }
-
-    @Configuration
-    @EnableResourceServer
-    public static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-
-        @Override
-        public void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable()
-                    .authorizeRequests()
-                    .antMatchers("/hello**").authenticated();
-        }
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/hello**").authenticated();
     }
 }
